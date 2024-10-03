f1fd1e132faf2e7570edf496cccc25a0d7227050
patricia <pattychela97@gmail.com>
2024-10-03 12:06:29 +0300

    solves no parameters

---
+++ noparameters.js
@@ -0,0 +1,7 @@
+// function to create a clock system that logs the current date and time 
+function logCurrentTime (){
+    let logCurrentTime = Date();
+    console.log (logCurrentTime);
+   
+}
+logCurrentTime ();
