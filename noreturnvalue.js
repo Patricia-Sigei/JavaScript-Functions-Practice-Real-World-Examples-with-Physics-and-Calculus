02668f77f9d07665767b49fd97cddf4bd2c2a3e8
patricia <pattychela97@gmail.com>
2024-10-03 12:25:47 +0300

    return no value function

---
+++ noreturnvalue.js
@@ -0,0 +1,8 @@
+// function to create a logging system for an app
+function logUserAction(userAction){
+console.log(`User Performed: "${userAction}"`);
+}
+logUserAction("Login");
+logUserAction("View Profile");
+logUserAction("Logout");
+logUserAction("Add Item to Cart");
